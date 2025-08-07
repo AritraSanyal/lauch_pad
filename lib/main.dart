da0ceed247d2e418289864cbee6d2ef77d47f346
Aritra Sanyal <aritras2003@gmail.com>
2025-08-07 16:26:08 +0530

    added blank lauchpad screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:launch_pad/pages/lauch_pad.dart';
 // import 'package:google_fonts/google_fonts.dart';
 // import 'package:audioplayers/audioplayers.dart';
 
@@ -11,6 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LauchPad(),
+    );
   }
 }
---
+++ lib/pages/lauch_pad.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LauchPad extends StatefulWidget {
+  const LauchPad({super.key});
+
+  @override
+  State<LauchPad> createState() => _LauchPadState();
+}
+
+class _LauchPadState extends State<LauchPad> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
