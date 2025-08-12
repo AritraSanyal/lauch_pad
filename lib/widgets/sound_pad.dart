1205366e53c6bd2cbc05b016bacb84b5fcc800a7
Aritra Sanyal <aritras2003@gmail.com>
2025-08-12 15:21:12 +0530

    added sounds to the pads
    changed the sound pad such that when tapped the associated sound gets played

--- lib/widgets/sound_pad.dart
+++ lib/widgets/sound_pad.dart
@@ -1,33 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class soundPad extends StatefulWidget {
   final String soundFile;
   final List<Color> colorList;
 
-  const soundPad({
-    super.key,
-    required this.soundFile,
-    required this.colorList
-  });
+  const soundPad({super.key, required this.soundFile, required this.colorList});
 
   @override
   State<soundPad> createState() => _soundPadState();
 }
 
 class _soundPadState extends State<soundPad> {
- 
+  // final _audioPlayer = AudioPlayer();
+  bool _isPlaying = false;
+
+  Future<void> _playSound() async {
+    setState(() {
+      _isPlaying = true;
+    });
+
+    //each tap gets its own AudioPlayer object
+    final _audioPlayer = AudioPlayer();
+    await _audioPlayer.play(AssetSource(widget.soundFile));
+    _audioPlayer.onPlayerComplete.listen((_) {
+      setState(() {
+        _isPlaying = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 90,
-      margin: EdgeInsets.all(1.5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        gradient: RadialGradient(
-          colors:widget.colorList,
-          radius: 0.5),
+    return GestureDetector(
+      onTap: _playSound,
+      child: Container(
+        height: 100,
+        width: 90,
+        margin: EdgeInsets.all(1.5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: _isPlaying ? Colors.white : null,
+          gradient: _isPlaying
+              ? null
+              : RadialGradient(colors: widget.colorList, radius: 0.5),
+        ),
       ),
     );
   }
